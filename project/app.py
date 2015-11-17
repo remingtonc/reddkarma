dd6a6bf1d5536a80fcda003f27b25a511b4e3620
RemingtonCampbell <rem.campbell@gmail.com>
2015-11-17 17:40:13 -0500

    BigQuery API implementation & output
    Implemented a BigQuery API handler and set default page to be a request
    to BigQuery for testing.

--- project/app.py
+++ project/app.py
@@ -1,11 +1,40 @@
 from flask import Flask
 from werkzeug.contrib.fixers import ProxyFix
+import bigquery
 
 app = Flask(__name__)
+bq = BigQuery()
+
+SELECT domain, COUNT(*) count, ROUND(AVG(score), 1) avg_score
+FROM [fh-bigquery:reddit_posts.full_corpus_201509]
+WHERE YEAR(SEC_TO_TIMESTAMP(created))=2015
+AND NOT domain CONTAINS 'self.'
+GROUP BY 1
+HAVING count>700
+ORDER BY 3 DESC
+LIMIT 100
 
 @app.route('/')
 def hello():
-	return "Hello world!"
+	preQuery = 'SELECT domain, COUNT(*) count, ROUND(AVG(score), 1) avg_score FROM'
+	postQuery = 'WHERE YEAR(SEC_TO_TIMESTAMP(created))=2015 AND NOT domain CONTAINS "self."" GROUP BY 1 HAVING count > 700 ORDER BY 3 DESC LIMIT 10'
+	queryString = bq.buildQuery(preQuery, postQuery)
+	result = bq.query(queryString)
+	result = QueryResult(result)
+	if (!result.querySuccess()):
+		return 'Query did not succeed.'
+	output = '<table><tr>'
+	headers = result.getSchema()
+	while (header = headers.next()):
+		output += '<th>' + header['name'] + '</th>'
+	output += '</tr>'
+	while (row = result.getResult()):
+		output += '<tr>'
+		while (val = row.next()):
+			output += '<td>' + val + '</td>'
+		output += '</tr>'
+	output += '</table>'
+	return output
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
---
+++ project/bigquery.py
@@ -0,0 +1,96 @@
+from apiclient.discovery import build
+import json
+
+class BigQuery:
+	""" Container for common methods related to BigQuery functionality.
+	Handles BigQuery access and retrieval.
+	"""
+
+	api_key = 'AIzaSyAJn5yLJlQUx7kB2N3xEXIK_D7yEq05JJw'
+	project_id = 'loyal-landing-110819'
+	table_id = '[loyal-landing-110819:reddit_posts.full_corpus_201509]'
+
+	# Basic structure of a request to the BigQuery API.
+	requestStructure = {
+		'timeoutMs': 10000,
+		'kind': 'bigquery#queryrequest',
+		'dryRun': False,
+		'useQueryCahce': True,
+		'defaultDataset': {
+			'projectId': project_id,
+			'datasetId': 'reddit_posts'
+		},
+		'maxResults': 100,
+		'query': ''
+	}
+
+	# Build service for common usage.
+	service = build('bigquery', 'v2', api_key)
+
+	def getTableId():
+		""" Get the table ID to be used for querying. """
+		return table_id
+
+	def buildQuery(prefix, suffix):
+		""" Build a query with the table ID.
+		:param prefix: Query fragment to append before table identification.  Trailing spaces not required.
+		"param suffix: Query fragment to append after table identification.  Padding spaces not required.
+		"""
+		return prefix + " " + table_id + " " + suffix;
+
+	def query(queryString):
+		""" Query the BigQuery database and return results synchronously
+		:param queryString: String to query BigQuery with.
+		"""
+		# Copy requestStructure so we don't run the risk of the structure changing
+		request = requestStructure.copy()
+		request['query'] = queryString
+		# Convert request dict into JSON
+		request = json.dumps(request)
+		# Make request, takes up to request.timeoutMs
+		response = service.jobs.query(project_id, request)
+		# Convert returned JSON into dict for return
+		return json.loads(response)
+
+class QueryResult:
+	""" Container for common methods related to BigQuery results.
+	Handles determining query successes, getting the schema, and getting results from returned set."
+	"""
+
+	def __init__(self, queryResult):
+		""" Initialize new QueryResult using supplied results. """
+		self.queryResult = queryResult
+		self.index = 0
+		self.numResults = 0
+		if (querySuccess())
+			self.numResults = int(queryResult['totalRows'])
+		self.numRows = len(queryResult['rows'])
+
+	def querySuccess():
+		""" Check if query was successful. """
+		return queryResult['jobComplete']
+
+	def hasErrors():
+		""" Check if query returned errors or warnings. """
+		return (len(queryResult['errors']) > 0)
+
+	def resetIndex():
+		""" Reset index of result feeder. """
+		index = 0
+
+	def getResult():
+		""" Get next result from query results.
+		Return False for unsuccessful query.
+		Return None for no more results.
+		"""
+		if (querySuccess())
+			return False
+		if (index == numRows - 1)
+			return None
+		index++
+		return queryResult['rows'].next()
+
+	def getSchema():
+		if (!querySuccess())
+			return False
+		return queryResult['schema']['fields']
