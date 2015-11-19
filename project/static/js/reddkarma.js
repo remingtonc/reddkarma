fdcfd6692bf2237b403a9d18d2d602b9f11ce408
RemingtonCampbell <rem.campbell@gmail.com>
2015-11-19 00:06:00 -0500

    Chart functioning, modifying aesthetics of site

--- project/static/js/reddkarma.js
+++ project/static/js/reddkarma.js
@@ -1,8 +1,8 @@
 $(document).ready(init);
 
 function init() {
-	getKarma();
-	initializeHourlyChart();
+        getKarma();
+        initializeHourlyChart();
 }
 
 /**
@@ -10,39 +10,41 @@ function init() {
  * and apply it to the web page.
  */
 function getKarma() {
-	$.get("/karma/"+subreddit).done(function(data) {
-		console.log("totalkarma: " + data.f[0].v);
-		$("#totalKarma").html(data.f[0].v);
-		console.log("averagekarma: " + data.f[1].v);
-		$("#avgKarma").html(data.f[1].v);
-	});
+        $.get("/karma/"+subreddit).done(function(data) {
+                data = JSON.parse(data);
+                console.log("totalkarma: " + data[0].f[0].v);
+                $("#totalKarma").html(data[0].f[0].v);
+                console.log("averagekarma: " + data[0].f[1].v);
+                $("#avgKarma").html(data[0].f[1].v);
+        });
 }
 
 /**
  * Initialize the hourly chart layout and content.
  */
 function initializeHourlyChart() {
-	var chartData = {
-		labels: [],
-		datasets: [
-			{
-				label: "Karma By Hour",
-				fillColor: "rgba(220,220,220,0.5)",
-	            strokeColor: "rgba(220,220,220,0.8)",
-	            highlightFill: "rgba(220,220,220,0.75)",
-	            highlightStroke: "rgba(220,220,220,1)",
-	            data: []
-	        }
-		]
-	};
-	for (var i = 0; i < 24; i++)
-		chartData.labels[i] = ((i==0 || i==12) ? 12 : (i % 12)) + " " + ((i > 11) ? "AM" : "PM");
-	var ctx = document.getElementById("hourlyChart").getContext("2d");
-	$.get("/hourly/"+subreddit).done(function(data) {
-		for (var i in data) {
-			console.log(i.f[1].v);
-			chartData.datasets[0].data.push(i.f[1].v);
-		}
-		var hourlyChart = new Chart(ctx).Bar(data, {});
-	});
+        var chartData = {
+                labels: [],
+                datasets: [
+	                {
+	                    label: "Karma By Hour",
+	                    fillColor: "rgba(151,187,205,0.5)",
+			            strokeColor: "rgba(151,187,205,0.8)",
+			            highlightFill: "rgba(151,187,205,0.75)",
+			            highlightStroke: "rgba(151,187,205,1)",
+	                    data: []
+	                }
+                ]
+        };
+        for (var i = 0; i < 24; i++)
+                chartData.labels[i] = ((i==0 || i==12) ? 12 : (i % 12)) + " " + ((i < 12) ? "AM" : "PM");
+        var ctx = document.getElementById("hourlyChart").getContext("2d");
+        $.get("/hourly/"+subreddit).done(function(data) {
+                data = JSON.parse(data);
+                for (var i in data) {
+                        console.log(data[i].f[1].v);
+                        chartData.datasets[0].data.push(data[i].f[1].v);
+                }
+                var hourlyChart = new Chart(ctx).Bar(chartData, {});
+        });
 }
