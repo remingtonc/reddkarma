d10fd22e512bc183e7975c31156de181afeace0c
RemingtonCampbell <rem.campbell@gmail.com>
2015-11-19 02:04:17 -0500

    Homepage linking and aesthetics

--- project/static/js/home.js
+++ project/static/js/home.js
@@ -3,7 +3,7 @@ function init() {
 		data = JSON.parse(data);
 		outputStr = "";
 		for (var i in data)
-			outputStr += "<tr><td>"+data[i].f[0].v+"</td><td>"+data[i].f[1].v+"</td><td>"+data[i].f[2].v+"</td></tr>";
+			outputStr += "<tr><td><a href=\"http://www.reddit.com/r/"+data[i].f[0].v+"\">/r/"+data[i].f[0].v+"</a></td><td>"+data[i].f[1].v+"</td><td>"+data[i].f[2].v+"</td></tr>";
 		$("#karmaFactories").append(outputStr);
 	});
 }
